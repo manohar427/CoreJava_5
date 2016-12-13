40163ad2004d8ae60c5e1b009478e8a830de552a
Tester <Tester@email.com>
2016-12-13 08:19:53 +0530

    Dec 13 Class string

---
+++ StringDemo/src/com/test/StringDemo.java
@@ -0,0 +1,65 @@
+package com.test;
+
+public class StringDemo {
+	
+	public static void main(String[] args) {
+		//obj.add();
+       /*String name = "xyz";
+       
+       String name1 = new String("xyz");
+       String name2 = new String("xyz");
+       
+       String names = "xyz";
+       
+       System.out.println("Name:"+name);
+       System.out.println("Name1:"+name1);
+       
+       System.out.println(name1==name);
+       System.out.println(name==names);
+       System.out.println(name1==name2);*/
+		
+		
+		/*String personName= "Abcsahdahsdfashgdfgsah0";
+		
+		
+		System.out.println(personName.length());
+		
+		System.out.println(personName.toUpperCase());
+		System.out.println(personName.toLowerCase());
+		
+		
+		String name3 = "Abcsahdahsdfashgdfgsah";
+		
+		
+		//System.out.println(personName.equals(name3));
+		
+		for (int i=0;i<personName.length();i++){
+			
+			System.out.println(personName.charAt(i));
+		}
+		
+		
+		personName = personName.replaceAll("a", "A");
+		
+		System.out.println(personName);
+		*/
+		
+		
+		String xyz = "Abc";
+		System.out.println(xyz.hashCode());
+		xyz = xyz+"d";
+		
+		System.out.println(xyz.hashCode());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
